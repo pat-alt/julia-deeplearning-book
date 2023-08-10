bd485182c4e5f6db17bc7f542e8985dc9cfed0eb
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-08-10 12:47:24 +0200

    added CondaPkg back in

--- setup_render.jl
+++ setup_render.jl
@@ -1,8 +1,11 @@
-@info "Render book"
 using Pkg
 Pkg.activate(@__DIR__)
 Pkg.status()
 Pkg.resolve()
 Pkg.instantiate()
-Pkg.build("IJulia") # build IJulia to the right version.
-run(`quarto render`)
+using CondaPkg
+CondaPkg.withenv() do
+    @info "Render book"
+    Pkg.build("IJulia") # build IJulia to the right version.
+    run(`quarto render`)
+end
