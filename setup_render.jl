846c33f069ed0de877e9b5c5154437de61af36b9
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-08-11 08:53:15 +0200

    please

--- setup_render.jl
+++ setup_render.jl
@@ -1,10 +1,10 @@
 using Pkg
 Pkg.activate(@__DIR__)
 Pkg.status()
-Pkg.resolve()
-Pkg.instantiate()
 using CondaPkg
 CondaPkg.withenv() do
+    Pkg.resolve()
+    Pkg.instantiate()
     @info "Render book"
     Pkg.build("IJulia") # build IJulia to the right version.
     run(`quarto render`)
